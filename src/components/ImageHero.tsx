171cbd6817b2ad912025325bf8ea4c37d3e6567a
Vinicius de Alencar Freire <alencarfreire@gmail.com>
2025-02-02 14:53:27 -0300

    att hero and add carrousel images

---
+++ src/components/ImageHero.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const images = [
+  "./img06.jpg",
+  "./img02.jpg",
+  "./img04.jpg",
+  "./img01.jpg",
+  "./img03.jpg",
+  "./img05.jpg",
+];
+
+export default function ImgHeader() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <img
+      src={images[index]}
+      alt="Imagem do Cabeçalho"
+      width="650"
+      height="550"
+      className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last"
+      loading="lazy"
+    />
+  );
+}
